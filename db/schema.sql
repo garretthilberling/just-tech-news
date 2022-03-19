f7dcd6969a0613a3f46612017ec694b61da22408
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-19 11:43:38 -0400

    users can now comment!

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,26 @@
-DROP DATABASE IF EXISTS just_tech_news_db;
+-- DROP DATABASE IF EXISTS just_tech_news_db;
 
-CREATE DATABASE just_tech_news_db;
+-- CREATE DATABASE just_tech_news_db;
 -- USE just_tech_news_db;
+
+-- INSERT INTO user (username, email, password) 
+-- VALUES
+-- ('name1','name1@gmail.com','1234567'),
+-- ('name2','name2@gmail.com','1234567'),
+-- ('name3','name3@gmail.com','1234567');
+
+-- INSERT INTO post (title, post_url, user_id) 
+-- VALUES
+-- ('DOGE just mooned bro','https://google.com/dogemoonlanding',1),
+
+-- {
+--     "title": "DOGE just mooned bro",
+--     "post_url": "https://google.com/dogemoonlanding",
+--     "user_id": 1
+-- }
+
+-- {
+--     "comment_text": "Selling my house rn!!!",
+--     "user_id": 3,
+--     "post_id": 1
+-- }
