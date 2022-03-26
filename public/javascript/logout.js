8c4c5b6f86dc9a6e09272267a1738a5756855b2e
garretthilberling <ghilberling@knights.ucf.edu>
2022-03-25 22:45:47 -0400

    users can add comments and upvote. lo

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,8 +1,10 @@
-async function logout() {
+async function logout(event) {
+  event.preventDefault();
     const response = await fetch('/api/users/logout', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' }
     });
+    console.log('click');
   
     if (response.ok) {
       document.location.replace('/');
